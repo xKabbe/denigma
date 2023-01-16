85e9e592997ce31f7a390e4aab238b5d963e48d2
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-17 00:22:27 +0100

    #17 Apply first simple draft of a sidebar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,10 +1,117 @@
 // External imports
-import React from 'react';
+import React, { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Box, IconButton, Paper, Typography, useTheme } from '@mui/material';
+import { MenuOutlined as MenuOutlinedIcon } from '@mui/icons-material';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 // Local imports
+import SideBarTitle from './components/SideBarTitle/SideBarTitle';
+import logo from '../../assets/icons/denigma_icon.png';
+import { tokens } from '../../themes/theme';
 
 function SideBar() {
-  return <h1>SideBar is working!</h1>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        '& .pro-sidebar-inner': { background: `${colors.primary[400]} !important` },
+        '& .pro-icon-wrapper': { backgroundColor: 'transparent !important' },
+        '& .pro-inner-item': { padding: '5px 35px 5px 20px !important' },
+        '& .pro-inner-item:hover': { color: '#868dfb !important' },
+        '& .pro-menu-item.active': { color: '#6870fa !important' },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape='square'>
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: '10px 0 20px 0',
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box display='flex' justifyContent='space-between' alignItems='center' ml='15px'>
+                <img src={logo} alt='denigma_logo' width='70px' height='70px' />
+                <Typography variant='h4' fontWeight={700} color='#4bc9a2'>
+                  DeNigmA
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box
+              paddingLeft={isCollapsed ? undefined : '5%'}
+              paddingRight={isCollapsed ? undefined : '5%'}
+            >
+              <SideBarTitle title='Views' />
+              <Box margin={2}>
+                <Paper
+                  sx={{
+                    borderRadius: 5,
+                    paddingTop: 2,
+                    paddingBottom: 2,
+                    display: 'flex',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#4bc9a2',
+                  }}
+                >
+                  <Typography>Test</Typography>
+                </Paper>
+              </Box>
+
+              <SideBarTitle title='Sequence Settings' />
+              <Box margin={2}>
+                <Paper
+                  sx={{
+                    borderRadius: 5,
+                    paddingTop: 2,
+                    paddingBottom: 2,
+                    display: 'flex',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#4bc9a2',
+                  }}
+                >
+                  <Typography>Test</Typography>
+                </Paper>
+              </Box>
+
+              <SideBarTitle title='General Settings' />
+              <Box margin={2}>
+                <Paper
+                  sx={{
+                    borderRadius: 5,
+                    paddingTop: 2,
+                    paddingBottom: 2,
+                    display: 'flex',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#4bc9a2',
+                  }}
+                >
+                  <Typography>Test</Typography>
+                </Paper>
+              </Box>
+            </Box>
+          )}
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
 }
 
 export default SideBar;
