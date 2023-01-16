a20df71708e0025e1cb85929add58e555b14c86a
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-17 00:18:59 +0100

    #17 Add SearchBar and IconBar component

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,10 +1,18 @@
 // External imports
 import React from 'react';
+import { Box } from '@mui/material';
 
 // Local imports
+import SearchBar from './components/SearchBar/SearchBar';
+import IconBar from './components/IconBar/IconBar';
 
 function TopBar() {
-  return <h1>TopBar is working!</h1>;
+  return (
+    <Box display='flex' justifyContent='space-between' p={2}>
+      <SearchBar />
+      <IconBar />
+    </Box>
+  );
 }
 
 export default TopBar;
