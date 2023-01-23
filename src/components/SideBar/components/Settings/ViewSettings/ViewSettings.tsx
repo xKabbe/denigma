dbf718672d720cfe5522a85630b59a851a5de335
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-23 23:41:39 +0100

    #17 Add aria-label to slider components

--- src/components/SideBar/components/Settings/ViewSettings/ViewSettings.tsx
+++ src/components/SideBar/components/Settings/ViewSettings/ViewSettings.tsx
@@ -11,10 +11,10 @@ function ViewSettings() {
       <SideBarTitle title='View Settings' />
       <Box margin={2}>
         <Typography gutterBottom>Zoom</Typography>
-        <Slider color='secondary' />
+        <Slider aria-label='zoom' color='secondary' />
 
         <Typography gutterBottom>Size</Typography>
-        <Slider color='secondary' />
+        <Slider aria-label='size' color='secondary' />
       </Box>
     </Box>
   );
