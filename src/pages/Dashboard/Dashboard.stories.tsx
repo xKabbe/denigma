02998d3c49de25fe045dc97081715314536d68a1
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-25 23:13:00 +0100

    #17 Add Dashboard story

---
+++ src/pages/Dashboard/Dashboard.stories.tsx
@@ -0,0 +1,13 @@
+// External imports
+import { ComponentMeta } from '@storybook/react';
+
+// Local imports
+import Dashboard from './Dashboard';
+
+// Story placement in the story list
+export default {
+  title: 'pages/Dashboard',
+  component: Dashboard,
+} as ComponentMeta<typeof Dashboard>;
+
+export const Default = () => <Dashboard />;
