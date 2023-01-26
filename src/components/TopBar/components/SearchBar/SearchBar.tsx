fc41a76470752e7377043c652850ae80e3c6320b
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-26 23:30:47 +0100

    #17 Add under construction alert for search functionality

--- src/components/TopBar/components/SearchBar/SearchBar.tsx
+++ src/components/TopBar/components/SearchBar/SearchBar.tsx
@@ -5,10 +5,17 @@ import { Search as SearchIcon } from '@mui/icons-material';
 
 // Local imports
 import { tokens } from '../../../../themes/theme';
+import UnderConstructionSnackbar from '../../../UnderConstructionSnackbar/UnderConstructionSnackbar';
 
 function SearchBar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOnSearch = () => {
+    // TODO: Under Construction - Add functionality
+    setOpen(true);
+  };
 
   return (
     <Box
@@ -19,9 +26,17 @@ function SearchBar() {
       }}
     >
       <InputBase sx={{ ml: 2, flex: 1 }} placeholder='Search' />
-      <IconButton aria-label='search-button' type='button' sx={{ p: 1 }}>
+      <IconButton
+        aria-label='search-button'
+        type='button'
+        sx={{ p: 1 }}
+        onClick={() => handleOnSearch()}
+      >
         <SearchIcon />
       </IconButton>
+
+      {/* TODO: Under Construction - Add functionality */}
+      <UnderConstructionSnackbar open={open} setOpen={setOpen} />
     </Box>
   );
 }
