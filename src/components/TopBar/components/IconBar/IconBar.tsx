466b0c182f41a5df3c6236a5d75fc2e91bca9486
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-29 14:23:50 +0100

    #26 Add LanguageSelectionMenu component and remove example icon buttons

--- src/components/TopBar/components/IconBar/IconBar.tsx
+++ src/components/TopBar/components/IconBar/IconBar.tsx
@@ -1,31 +1,72 @@
 // External imports
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Box, IconButton, useTheme } from '@mui/material';
 import {
   LightModeOutlined as LightModeOutlinedIcon,
   DarkModeOutlined as DarkModeOutlinedIcon,
-  NotificationsOutlined as NotificationsOutlinedIcon,
-  SettingsOutlined as SettingsOutlinedIcon,
 } from '@mui/icons-material';
 
 // Local imports
 import { ColorContext } from '../../../../themes/theme';
+import LanguageFlagDE from '../../../../assets/icons/language/LanguageFlagDE';
+import LanguageFlagEN from '../../../../assets/icons/language/LanguageFlagEN';
+import LanguageFlagES from '../../../../assets/icons/language/LanguageFlagES';
+import LanguageSelectionMenu from './components/LanguageSelectionMenu.tsx/LanguageSelectionMenu';
+
+type Languages = 'de' | 'en' | 'es';
+
+const languageOptions = {
+  de: {
+    label: 'German',
+    component: <LanguageFlagDE size={20} />,
+  },
+  en: {
+    label: 'English',
+    component: <LanguageFlagEN size={20} />,
+  },
+  es: {
+    label: 'Spanish',
+    component: <LanguageFlagES size={20} />,
+  },
+};
+
+// type Languages = keyof typeof languageOptions;
 
 function IconBar() {
   const theme = useTheme();
   const colorMode = useContext(ColorContext);
+
+  const [currentLanguage, setCurrentLanguage] = useState<Languages>('en');
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpenLanguageSelection = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   return (
     <Box display='flex'>
       <IconButton aria-label='color-mode-button' onClick={colorMode.toggleColorMode}>
         {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
       </IconButton>
-      <IconButton aria-label='notification-button'>
-        <NotificationsOutlinedIcon />
+
+      <IconButton
+        aria-label='language-selection-button'
+        aria-controls={open ? 'language-selection-menu' : undefined}
+        aria-expanded={open ? 'true' : 'false'}
+        onClick={handleOpenLanguageSelection}
+      >
+        {languageOptions[currentLanguage].component}
       </IconButton>
-      <IconButton aria-label='settings-button'>
-        <SettingsOutlinedIcon />
-      </IconButton>
+
+      <LanguageSelectionMenu
+        open={open}
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        currentLanguage={currentLanguage}
+        setCurrentLanguage={setCurrentLanguage}
+        languageOptions={languageOptions}
+      />
     </Box>
   );
 }
