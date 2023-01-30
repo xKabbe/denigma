6e9c71cf14de97bc6b4907ad74115c7505c933a2
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-01-30 20:21:21 +0100

    #26 Add new LanguageSelectionMenu component to IconBar jest test

--- src/components/TopBar/components/IconBar/IconBar.test.tsx
+++ src/components/TopBar/components/IconBar/IconBar.test.tsx
@@ -1,5 +1,6 @@
 // External imports
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 
@@ -37,5 +38,37 @@ describe('IconBar', () => {
       const colorModeIcon = screen.getByTestId(colorMode.icon);
       expect(colorModeIcon).toBeInTheDocument();
     });
+  });
+
+  test('should render IconBar language menu and menuitems', () => {
+    render(<IconBar />, { wrapper: BrowserRouter });
+
+    const languageButton = screen.getByRole('button', { name: /language-selection-button/i });
+    expect(languageButton).toBeInTheDocument();
+    expect(languageButton.getAttribute('aria-expanded')).toEqual('false');
+
+    let germanMenuItem = screen.queryByRole('menuitem', { name: /german/i });
+    expect(germanMenuItem).toBeNull();
+
+    let englishMenuItem = screen.queryByRole('menuitem', { name: /english/i });
+    expect(englishMenuItem).toBeNull();
+
+    let spanishMenuItem = screen.queryByRole('menuitem', { name: /spanish/i });
+    expect(spanishMenuItem).toBeNull();
+
+    userEvent.click(languageButton);
+    expect(languageButton.getAttribute('aria-expanded')).toEqual('true');
+
+    const menu = screen.getByRole('menu');
+    expect(menu).toBeInTheDocument();
+
+    germanMenuItem = screen.getByRole('menuitem', { name: /german/i });
+    expect(germanMenuItem).toBeInTheDocument();
+
+    englishMenuItem = screen.getByRole('menuitem', { name: /english/i });
+    expect(englishMenuItem).toBeInTheDocument();
+
+    spanishMenuItem = screen.getByRole('menuitem', { name: /spanish/i });
+    expect(spanishMenuItem).toBeInTheDocument();
   });
 });
