7bcb4f394389287e05c49a895475f4adba555812
Steven Karbjinsky <steven.karbjinsky@web.de>
2023-02-05 02:35:10 +0100

    #19 Add further mock expect statement regarding sequence generation length

--- src/pages/Dashboard/SequenceVisualizer/components/GenerateSequenceButton/GenerateSequenceButton.test.tsx
+++ src/pages/Dashboard/SequenceVisualizer/components/GenerateSequenceButton/GenerateSequenceButton.test.tsx
@@ -7,10 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 import GenerateSequenceButton from './GenerateSequenceButton';
 
 const generateSequenceOptions = [
-  'Generate Sequence',
-  'Generate Sequence (Small)',
-  'Generate Sequence (Medium)',
-  'Generate Sequence (Large)',
+  { label: 'Generate Sequence', sequenceLength: { min: 1, max: 10000 } },
+  { label: 'Generate Sequence (Small)', sequenceLength: { min: 1, max: 100 } },
+  { label: 'Generate Sequence (Medium)', sequenceLength: { min: 101, max: 1000 } },
+  { label: 'Generate Sequence (Large)', sequenceLength: { min: 1001, max: 10000 } },
 ];
 
 const mockRandomSequenceGenerator = jest.fn();
@@ -41,7 +41,8 @@ describe('GenerateSequenceButton', () => {
     expect(generateSequenceButton).toBeInTheDocument();
 
     userEvent.click(generateSequenceButton);
-    expect(mockRandomSequenceGenerator).toHaveBeenCalled();
+    expect(mockRandomSequenceGenerator).toBeCalled();
+    expect(mockRandomSequenceGenerator).toBeCalledWith({ max: 10000, min: 1 });
   });
 
   test('should render generate sequence options button click', () => {
@@ -57,13 +58,17 @@ describe('GenerateSequenceButton', () => {
     userEvent.click(generateSequenceOptionsButton);
 
     generateSequenceOptions.forEach((generateSequenceOption) => {
-      const option = screen.getByRole('menuitem', { name: generateSequenceOption });
+      const option = screen.getByRole('menuitem', { name: generateSequenceOption.label });
       expect(option).toBeInTheDocument();
 
       userEvent.click(option);
 
-      const generateSequenceButton = screen.getByRole('button', { name: generateSequenceOption });
+      const generateSequenceButton = screen.getByRole('button', { name: generateSequenceOption.label });
       expect(generateSequenceButton).toBeInTheDocument();
+
+      userEvent.click(generateSequenceButton);
+      expect(mockRandomSequenceGenerator).toBeCalled();
+      expect(mockRandomSequenceGenerator).toBeCalledWith(generateSequenceOption.sequenceLength);
     });
   });
 });
